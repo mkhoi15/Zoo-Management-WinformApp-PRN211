7ac7b13dd003f19bbb9c4211582df4a0212c09d4
lqviet45 <lqviet455@gmail.com>
2023-10-30 20:13:46 +0700

    adding delete in all repo

--- Repositories/CageRepository.cs
+++ Repositories/CageRepository.cs
@@ -4,5 +4,17 @@ namespace Repositories
 {
     public class CageRepository : RepositoryBase<Cage>
     {
-    }
+		public async Task<bool> DeleteCageAsync(Cage cage)
+		{
+			var cageDelete = await this.GetByIdAsync(cage);
+			if (cageDelete == null)
+			{
+				return false;
+			}
+			cageDelete.IsDeleted = true;
+			await _context.SaveChangesAsync();
+
+			return true;
+		}
+	}
 }
