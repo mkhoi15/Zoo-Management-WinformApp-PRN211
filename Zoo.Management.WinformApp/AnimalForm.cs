54f9f0fc704b6be62345fad624e794eb636608d0
mkhoi15 <khoivmse171254@fpt.edu.vn>
2023-10-30 14:24:49 +0700

    create Form

---
+++ Zoo.Management.WinformApp/AnimalForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zoo.Management.WinformApp
+{
+	public partial class AnimalForm : Form
+	{
+		public AnimalForm()
+		{
+			InitializeComponent();
+		}
+
+		private void btnCreate_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnUpdate_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnDelete_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
