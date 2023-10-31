14452bf16adbdc3ded9426f2ed7412e1e15a5aea
TinKOA <phatnguyenhoang69@gmail.com>
2023-11-01 03:04:08 +0700

    Add AreaForm

---
+++ Zoo.Management.WinformApp/AreaForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Zoo.Management.WinformApp
+{
+    partial class AreaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvArea = new DataGridView();
+            lbArea = new Label();
+            lbId = new Label();
+            lbName = new Label();
+            txtId = new TextBox();
+            txtName = new TextBox();
+            btnCreate = new Button();
+            btnDelete = new Button();
+            btnUpdate = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvArea).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvArea
+            // 
+            dgvArea.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvArea.Location = new Point(358, 50);
+            dgvArea.Name = "dgvArea";
+            dgvArea.RowTemplate.Height = 25;
+            dgvArea.Size = new Size(417, 285);
+            dgvArea.TabIndex = 0;
+            dgvArea.CellDoubleClick += dgvArea_CellDoubleClick;
+            // 
+            // lbArea
+            // 
+            lbArea.AutoSize = true;
+            lbArea.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lbArea.Location = new Point(105, 9);
+            lbArea.Name = "lbArea";
+            lbArea.Size = new Size(112, 47);
+            lbArea.TabIndex = 1;
+            lbArea.Text = "AREA";
+            // 
+            // lbId
+            // 
+            lbId.AutoSize = true;
+            lbId.Location = new Point(72, 105);
+            lbId.Name = "lbId";
+            lbId.Size = new Size(17, 15);
+            lbId.TabIndex = 2;
+            lbId.Text = "Id";
+            // 
+            // lbName
+            // 
+            lbName.AutoSize = true;
+            lbName.Location = new Point(50, 149);
+            lbName.Name = "lbName";
+            lbName.Size = new Size(39, 15);
+            lbName.TabIndex = 3;
+            lbName.Text = "Name";
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(117, 97);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(100, 23);
+            txtId.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(117, 146);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(100, 23);
+            txtName.TabIndex = 5;
+            // 
+            // btnCreate
+            // 
+            btnCreate.Location = new Point(12, 204);
+            btnCreate.Name = "btnCreate";
+            btnCreate.Size = new Size(75, 23);
+            btnCreate.TabIndex = 6;
+            btnCreate.Text = "Create";
+            btnCreate.UseVisualStyleBackColor = true;
+            btnCreate.Click += btnCreate_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(117, 204);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 7;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(225, 206);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(75, 23);
+            btnUpdate.TabIndex = 8;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // AreaForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnDelete);
+            Controls.Add(btnCreate);
+            Controls.Add(txtName);
+            Controls.Add(txtId);
+            Controls.Add(lbName);
+            Controls.Add(lbId);
+            Controls.Add(lbArea);
+            Controls.Add(dgvArea);
+            Name = "AreaForm";
+            Text = "AreaForm";
+            ((System.ComponentModel.ISupportInitialize)dgvArea).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvArea;
+        private Label lbArea;
+        private Label lbId;
+        private Label lbName;
+        private TextBox txtId;
+        private TextBox txtName;
+        private Button btnCreate;
+        private Button btnDelete;
+        private Button btnUpdate;
+    }
+}
---
+++ Zoo.Management.WinformApp/AreaForm.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entities.Helper;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace Zoo.Management.WinformApp
+{
+    public partial class AreaForm : Form
+    {
+
+        private readonly AreaRepository _areaRepository;
+        public AreaForm()
+        {
+            InitializeComponent();
+
+            _areaRepository = new AreaRepository();
+
+            GetDataForDataGridView();
+
+
+        }
+
+        private void dgvArea_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnUpdate.Enabled = true;
+            btnDelete.Enabled = true;
+            btnCreate.Enabled = false;
+
+            var row = dgvArea.Rows[e.RowIndex];
+            txtId.Text = row.Cells[0].Value.ToString();
+            txtName.Text = row.Cells[1].Value.ToString();
+
+        }
+        private bool CheckValidation(Area area)
+        {
+            AreaValidationHelper validator = new();
+            var result = validator.Validate(area);
+
+            if (!result.IsValid)
+            {
+                var errorsMessage = result.ToString("\n");
+                MessageBox.Show(errorsMessage);
+                return false;
+            }
+            return true;
+        }
+        private int CheckValidId()
+        {
+            if (!int.TryParse(txtId.Text, out int id))
+            {
+                MessageBox.Show("Id must be integer");
+                return -1;
+            }
+            if (txtId.Text == "0")
+            {
+                MessageBox.Show("Id must be positive");
+                return 0;
+            }
+            return id;
+        }
+        private void ClearText()
+        {
+            txtId.Text = "";
+            txtName.Text = "";
+        }
+
+        private void GetDataForDataGridView()
+        {
+            var areaList = _areaRepository.GetAll().Where(c => !c.IsDeleted).Include(c => c.Cages).ToList();
+            if (areaList.Count > 0 && areaList is not null)
+            {
+                dgvArea.DataSource = areaList.Select(c => new
+                {
+                    id = c.Id,
+                    Name = c.Name,
+                }).ToList();
+            }
+        }
+
+        private async void btnCreate_Click(object sender, EventArgs e)
+        {
+            btnCreate.Enabled = false;
+            try
+            {
+                var areaName = txtName.Text;
+
+
+                var newArea = new Area()
+                {
+                    Name = areaName,
+                };
+
+                if (!CheckValidation(newArea)) return;
+
+                await _areaRepository.AddAsync(newArea);
+                GetDataForDataGridView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                ClearText();
+                btnCreate.Enabled = true;
+            }
+        }
+        private async void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btnUpdate.Enabled = false;
+
+                var id = CheckValidId();
+                if (id <= 0) return;
+
+                var area = _areaRepository.GetAll().Where(c => c.Id == id && !c.IsDeleted).FirstOrDefault();
+                if (area is null)
+                {
+                    MessageBox.Show("Area is not found. Can not update!");
+                    btnUpdate.Enabled = true;
+                    return;
+                }
+
+                area.Name = txtName.Text;
+
+                if (!CheckValidation(area))
+                {
+                    btnUpdate.Enabled = true;
+                    return;
+                }
+                await _areaRepository.UpdateAsync(area);
+
+                GetDataForDataGridView();
+
+                ClearText();
+                btnCreate.Enabled = true;
+                btnDelete.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                btnUpdate.Enabled = true;
+            }
+        }
+
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btnDelete.Enabled = false;
+
+                var id = CheckValidId();
+                if (id <= 0) return;
+
+                var deleteArea = new Area();
+
+                var name = txtName.Text;
+
+
+                if (name == "")
+                {
+                    deleteArea = _areaRepository.GetAll().Where(c => c.Id == id).FirstOrDefault();
+                }
+                else
+                {
+
+                    deleteArea = _areaRepository.GetAll().Where(c => c.Id == id
+                                                                && c.Name == name).FirstOrDefault();
+                }
+
+                if (deleteArea is null)
+                {
+                    MessageBox.Show("Area is not found. Can not delete!");
+                    btnDelete.Enabled = true;
+                    return;
+                }
+                if (deleteArea.IsDeleted)
+                {
+                    MessageBox.Show("Area is deleted. Can not delete again!");
+                    btnDelete.Enabled = true;
+                    return;
+                }
+
+                deleteArea.IsDeleted = true;
+
+                await _areaRepository.UpdateAsync(deleteArea);
+
+                GetDataForDataGridView();
+
+                ClearText();
+                btnCreate.Enabled = true;
+                btnUpdate.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                btnDelete.Enabled = true;
+            }
+        }
+    }
+}
