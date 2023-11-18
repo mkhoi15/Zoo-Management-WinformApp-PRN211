61cbb6c6ca7cbfed778b4aaabe4b78a9da38c420
mkhoi15 <khoivmse171254@fpt.edu.vn>
2023-11-18 09:29:16 +0700

    Add CageService GetAll

---
+++ Services/CageService.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Models;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+	public class CageService
+	{
+		private readonly CageRepository _cageRepository;
+
+		public CageService()
+		{
+			_cageRepository = new CageRepository();
+		}
+
+		public List<Cage> GetAll()
+		{
+			return _cageRepository.GetAll().ToList();
+		}
+
+	}
+}
